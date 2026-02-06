720f6eca970660132293badfb305d122eb6d2cf5
tojidi-debug <tojidi@gmail.com>
2026-02-06 20:10:45 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const designData = [
+    {
+        id: 1,
+        title: "추천 디자인",
+        img: "images/design1.jpg", 
+        prompt: "여기에 1번 디자인 프롬프트를 입력하세요."
+    },
+    {
+        id: 2,
+        title: "Light 스타일",
+        img: "images/design2.jpg",
+        prompt: "여기에 2번 디자인 프롬프트를 입력하세요."
+    },
+    {
+        id: 3,
+        title: "Collage 스타일",
+        img: "images/design3.jpg",
+        prompt: "여기에 3번 디자인 프롬프트를 입력하세요."
+    }
+];
